95908a36bd813a12ed2e422bf96f59c3bc4c567d
Railton Perenha <34631338+railtonperenha@users.noreply.github.com>
2023-06-30 20:27:48 -0300

    Exercícios complementares

--- src/exercicios/exercicio04.java
+++ src/exercicios/exercicio04.java
@@ -7,14 +7,19 @@ public class exercicio04 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int x = 0;
-		
-		System.out.printf("Digite um número: ");
-		x = sc.nextInt();
-		
-		
-		sc.close();
-
-	}
-
+		System.out.print("Digite um número inteiro: ");
+        int numero = sc.nextInt();
+        
+        System.out.println("Números pares:");
+        for (int i = 2; i <= numero; i += 2) {
+            System.out.print(i + " ");
+        }
+        
+        System.out.println("\nNúmeros ímpares:");
+        for (int i = 1; i <= numero; i += 2) {
+            System.out.print(i + " ");
+        }
+        
+        sc.close();
+    }
 }
